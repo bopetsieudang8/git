0f45162aea90e20a47c93337ad6ec6f264116dcf
Anh Duy <steven.duy95@gmail.com>
2016-12-11 13:30:32 +0700

    Day la file lien he

---
+++ lienhe.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d772.2198098543402!2d106.6622455005581!3d10.738729218125284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752e5aeda2bd91%3A0x6fcf33335878968b!2zNTQvMSBCw7RuZyBTYW8sIHBoxrDhu51uZyA1LCBRdeG6rW4gOCwgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1480995021495" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe><div>
+<div><iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fkopites1995&width=450&height=80&layout=standard&size=small&show_faces=true&appId=1370415296331931" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></div>
+<div><iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Ftienminh163&width=450&height=80&layout=standard&size=small&show_faces=true&appId=1370415296331931" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe></div>
+</body>
+</html>
